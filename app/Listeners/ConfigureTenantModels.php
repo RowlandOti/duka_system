04104156dde29ccb4b96e01e9de98603091f27cf
Otieno Rowland <r.otieno@kokonetworks.com>
2020-07-25 14:32:41 +0300

    fix: don't migrate tenants when deleted

--- app/Listeners/ConfigureTenantModels.php
+++ app/Listeners/ConfigureTenantModels.php
@@ -3,21 +3,34 @@
 namespace App\Listeners;
 
 use App\User;
+use Laravel\Passport\AuthCode;
+use Laravel\Passport\Client;
+use Laravel\Passport\PersonalAccessClient;
+use Laravel\Passport\Token;
 use Tenancy\Affects\Connections\ConnectionResolver;
 use Tenancy\Affects\Models\Events\ConfigureModels;
 
 class ConfigureTenantModels
 {
-    protected $model = User::class;
+
+    protected $models = [
+        'passportClient' => Client::class,
+        'passportToken' => Token::class,
+        'passportAuthCode' => AuthCode::class,
+        'passportAccessClient' => PersonalAccessClient::class,
+    ];
 
     public function handle(ConfigureModels $event)
     {
-        /*if($event->event->tenant)
+        if($event->event->tenant)
         {
-            ConfigureModels::setConnectionResolver(
-                $this->model,
-                new ConnectionResolver('sqlite', resolve('db'))
-            );
-        }*/
+            foreach ($this->models as $model)
+            {
+                ConfigureModels::setConnectionResolver(
+                    $model,
+                    new ConnectionResolver('mysql', resolve('db'))
+                );
+            }
+        }
     }
 }
